9f4a52ab4e9684327c8761bbaa5b6f506f43d231
Oneide Luiz Schneider <oneidels@gmail.com>
2020-01-21 22:40:45 -0300

    add user, topic and Auth

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -7,6 +7,7 @@ defmodule DiscussWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug DiscussWeb.Plugs.SetUser
   end
 
   pipeline :api do
@@ -19,7 +20,15 @@ defmodule DiscussWeb.Router do
     get "/", PageController, :index
 
     resources "/topic", TopicController
+    resources "/user", UserController
+  end
 
+  scope "/auth", DiscussWeb do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
   end
 
   # Other scopes may use custom stacks.
